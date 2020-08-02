3b64c0d89aeabe94961d6ac3106aa294ae5146f9
Naseem Mohammed <m.naseem@outlook.com>
2020-08-02 09:24:41 +0530

    Update basefile.tf

--- basefile.tf
+++ basefile.tf
@@ -1,37 +1,70 @@
- terraform {
-  required_version = ">= 0.11" 
- backend "azurerm" {
-  storage_account_name = "__terraformstorageaccount__"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-	access_key  ="__storagekey__"
-  features{}
-	}
-	}
-  provider "azurerm" {
-    version = "=2.0.0"
-features {}
-}
-resource "azurerm_resource_group" "dev" {
-  name     = "PULTerraform"
-  location = "West Europe"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_app_service_plan" "dev" {
-  name                = "__appserviceplan__"
-  location            = "${azurerm_resource_group.dev.location}"
-  resource_group_name = "${azurerm_resource_group.dev.name}"
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
+}
 
-  sku {
-    tier = "Free"
-    size = "F1"
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+  address_prefixes     = ["10.1.0.0/22"]
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.internal.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+
+    azure_policy {
+      enabled = false
+    }
+
+    http_application_routing {
+      enabled = false
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+
+    oms_agent {
+      enabled = false
+    }
   }
 }
 
-resource "azurerm_app_service" "dev" {
-  name                = "__appservicename__"
-  location            = "${azurerm_resource_group.dev.location}"
-  resource_group_name = "${azurerm_resource_group.dev.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
-
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name                  = "user"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+  vnet_subnet_id        = azurerm_subnet.internal.id
 }
